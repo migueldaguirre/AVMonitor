f48eb4251c4f083eaa8981e0493c97631707d439
Miguel Aguirre <md.aguim@gmail.com>
2018-05-15 10:12:20 -0500

    Added Create and Edit for records (Registros)

--- app/controllers/registros_controller.rb
+++ app/controllers/registros_controller.rb
@@ -9,18 +9,32 @@ class RegistrosController < ApplicationController
   end
 
   def new
+    @registro = Registro.new
   end
 
   def create
+    registro = Registro.create(registro_params)
+    redirect_to registro_path(registro)
   end
 
   def edit
+    @registro = Registro.find(params[:id])
   end
 
   def update
+    @registro = Registro.find(params[:id])
+    @registro.update(params[:registro])
+    redirect_to registros_path
+
   end
 
   def destroy
+  end
+
+  private
+
+  def registro_params
+    params.require(:registro).permit(:producto, :ubicacion, :precio)
   end
 
 end
