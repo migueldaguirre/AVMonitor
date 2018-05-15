10993a8d221104fe2850681acb1de405b7111ffc
Miguel Aguirre <md.aguim@gmail.com>
2018-05-15 07:49:52 -0500

    Seed realizado

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Registro.create(ubicacion:"Urabá", producto: "Nominee", nivel: "Dist", negociacion: "Cont", precio: 50000)
+Registro.create(ubicacion:"Urabá", producto: "Grammya", nivel: "Alma", negociacion: "Cont", precio: 45000)
+Registro.create(ubicacion:"Urabá", producto: "Baikal", nivel: "Alma", negociacion: "Cont", precio: 48000)
+Registro.create(ubicacion:"Urabá", producto: "Nominee", nivel: "Usua", negociacion: "Cred", precio: 55000)
