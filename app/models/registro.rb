9ba37b7c8bedbb7871228c2defe9ec5c02cccecf
Miguel Aguirre <md.aguim@gmail.com>
2018-06-28 17:45:24 -0500

    Fixed Registro Create form, activating all current parameters in params routing

--- app/models/registro.rb
+++ app/models/registro.rb
@@ -2,6 +2,6 @@ class Registro < ApplicationRecord
   validates :precio, presence: true
   validates :producto, presence: true
   validates :ubicacion, presence: true
-  # validates :negociacion, presence: true
+  validates :negociacion, presence: true
   # validates :nivel, presence: true
 end
