f9ac72746ad8c3ec920f5b28098ed514119e3dcc
Miguel Aguirre <md.aguim@gmail.com>
2018-06-30 23:22:41 -0500

    Now all registros, when created, link the parameters of current_user. They can be accesed via Registro.user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180515105625) do
+ActiveRecord::Schema.define(version: 20180630215919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20180515105625) do
     t.boolean "aprobado"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_registros_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,8 +43,12 @@ ActiveRecord::Schema.define(version: 20180515105625) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.integer "cc"
+    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "registros", "users"
 end
