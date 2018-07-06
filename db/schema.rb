0d47c940f62d2a22dd9156ace9b72e5fa18fd9d9
Miguel Aguirre <md.aguim@gmail.com>
2018-07-05 23:14:51 -0500

    New registro form fixed and ready

--- db/schema.rb
+++ db/schema.rb
@@ -57,9 +57,9 @@ ActiveRecord::Schema.define(version: 20180706023308) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.integer "cc"
+    t.string "cc"
     t.string "role"
-    t.boolean "admin"
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
