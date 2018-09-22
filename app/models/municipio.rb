b84e93e612628333cf1a454c8620d0d3a724f435
Miguel Aguirre <md.aguim@gmail.com>
2018-09-22 02:26:38 -0400

    Lista de municipios, realizada y funcionando.

--- app/models/municipio.rb
+++ app/models/municipio.rb
@@ -2,9 +2,13 @@ class Municipio < ApplicationRecord
   has_many :registros, dependent: :destroy
 
 
-def municipio_departamento
-    "#{nombre_municipio}, (#{departamento})"
-end
+  def municipio_departamento
+      "#{nombre_municipio}, (#{departamento})"
+  end
+
+  def departamento_municipio
+    "#{departamento}, (#{municipio})"
+  end
 
 end
 
