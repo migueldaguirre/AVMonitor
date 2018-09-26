10da907acb69136ac0d250c6f2779ba9e54e022c
Miguel Aguirre <md.aguim@gmail.com>
2018-09-25 23:54:11 -0400

    Users can now change their passwords

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   get 'producto/index'
 
-  devise_for :users
+  devise_for :users, :skip => [:registrations]
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-
+  as :user do
+  get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+  put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
 
   resources :registros
   # # List all records (registros)
